20f24f5862b8ede873818f7c4ae9628a2f83771d
d-n1sh <21012612@myrp.edu.sg>
2023-01-03 15:02:16 +0800

    extra

--- web/index.php
+++ web/index.php
@@ -1,3 +1,12 @@
-<?php
-
-echo "Hello World!";
+<!-- Fix: Added lang="en" -->
+<html lang="en">
+<head>
+    <!-- Fix: Added title -->
+    <title>Title</title>
+    <!-- Fix: Added a viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head> 
+<body>
+    Hello World!
+</body>
+</html>
